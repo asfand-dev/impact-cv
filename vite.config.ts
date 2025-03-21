9d9fd59ddfd140df69dce20ecedf841fbb91920f
asfand-dev <asfand.germany@gmail.com>
2025-03-21 14:48:51 +0100

    Added Github workflow to publish the build files to Github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
+  base: process.env.NODE_ENV === 'production' ? '/impact-cv/' : '/',
   server: {
     host: "::",
     port: 3000,
